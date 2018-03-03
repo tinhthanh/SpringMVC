8eccb68215477c3bfc751154393f33bd21e2db7a
tinhthanh <tinhthanh>
2018-03-03 15:18:27 +0700

    using Session Factory hibernate4

--- src/main/java/com/palltech/controller/HomeController.java
+++ src/main/java/com/palltech/controller/HomeController.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package com.palltech.controller;
+import com.palltech.model.User;
+import com.palltech.service.UserService;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,9 +20,12 @@ import org.springframework.ui.ModelMap;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    @Autowired
+    UserService userService;
     @RequestMapping(method = RequestMethod.GET)
     public String index(ModelMap model){
-        model.addAttribute("message", "Spring MVC XML Config Example");
+        Optional<User>  user =   this.userService.getUserByUserName("thanh");
+        model.addAttribute("message", "Spring MVC XML Config Example"  + user.get().getUsername());
         return "index";
     }
 }
--- src/main/java/com/palltech/model/User.java
+++ src/main/java/com/palltech/model/User.java
@@ -33,6 +33,38 @@ public class User {
 
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
   private Set<Authorities> authorities = new HashSet<>();
-
   //Getter and Setter methods
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<Authorities> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Authorities> authorities) {
+        this.authorities = authorities;
+    }
+  
 }
